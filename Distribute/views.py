aea98789a1ce5b31b9e301b2e3fae3ca925e34ad
Ryler <Ryler.heyman@icloud.com>
2025-02-23 11:35:44 -0800

    Finished the client page and made it so that you can see the selected message template and selected clients on the homepage of the Distribute app. I think I will implement the functionality for it next.

--- Distribute/views.py
+++ Distribute/views.py
@@ -6,7 +6,8 @@ from .models import MessageHistory, MessageHistoryCreator
 def index(request):
     text_message = request.session.get('text_message', '')
     text_message = MessageTemplate.objects.get(id=text_message)
-    client_list = request.session.get('client_list', '')
+    client_ids = request.session.get('client_list', '')
+    client_list = [Client.objects.get(id=int(client_id)) for client_id in client_ids]
     return render(request, 'Distribute/index.html', context={'text_message': text_message, 'client_list': client_list})
 
 def select_message(request):
@@ -17,5 +18,9 @@ def select_message(request):
     return render(request, 'Distribute/selectmessage.html', context={'messages': MessageTemplate.objects.all()})
 
 def select_clients(request):
-    return render(request, 'Distribute/selectclients.html')
+    if request.method == "POST":
+        client_list = request.POST.getlist('client-checkbox')
+        request.session['client_list'] = client_list
+        return redirect('Distribute:index')
+    return render(request, 'Distribute/selectclients.html', context={'clients': Client.objects.all()})
 
